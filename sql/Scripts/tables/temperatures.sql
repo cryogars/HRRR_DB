9f11c6cbf98eca0f5ad3d87fe1fa6e7096b250b7
Joachim Meyer <jmeyer@boisestate.edu>
2024-04-29 16:40:12 -0600

    SQL - Update Temperature table; add data_source column
    Also updates to have all columns required (NOT NULL)

--- sql/Scripts/tables/temperatures.sql
+++ sql/Scripts/tables/temperatures.sql
@@ -1,8 +1,9 @@
 DROP TABLE temperatures;
 CREATE TABLE public.temperatures (
-    site_id int4 NULL,
-    datetime timestamptz NULL,
-    value float4 NULL,
-    CONSTRAINT temperatures_site_id_datetime_key UNIQUE (site_id, datetime),
+    site_id int4 NOT NULL,
+    datetime timestamptz NOT NULL,
+    data_source public."data_source" NOT NULL,
+    value float4 NOT NULL,
+    PRIMARY KEY (site_id, data_source, datetime),
     CONSTRAINT temperatures_site_id_fkey FOREIGN KEY (site_id) REFERENCES public.sites(id)
 );
